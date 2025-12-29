0ba78667af8bdf3a928b3ef7b636b1391ff6d6ba
Soufiane-KS <soufiane.kousta@uit.ac.ma>
2025-12-29 14:31:50 +0100

    added gateway service

---
+++ gateway-service/src/test/java/com/kousta/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kousta.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
