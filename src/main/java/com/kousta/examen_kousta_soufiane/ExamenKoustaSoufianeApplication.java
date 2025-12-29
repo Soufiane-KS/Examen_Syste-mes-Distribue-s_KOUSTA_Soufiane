50aed4cd01e27a1004e2d5d9d132a3b401cead74
Soufiane-KS <soufiane.kousta@uit.ac.ma>
2025-12-29 14:23:43 +0100

    added config service

---
+++ src/main/java/com/kousta/examen_kousta_soufiane/ExamenKoustaSoufianeApplication.java
@@ -0,0 +1,13 @@
+package com.kousta.examen_kousta_soufiane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenKoustaSoufianeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenKoustaSoufianeApplication.class, args);
+    }
+
+}
